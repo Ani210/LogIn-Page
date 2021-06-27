c2ffe45a8989e6fa0cd6097395f746099b34ee4e
Anini Mishra <anini.mishra0611@gmail.com>
2021-06-27 21:54:19 +0530

    Complete website.

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ mongoose.set("useCreateIndex",true);
 const userSchema = new mongoose.Schema({
   email: String,
   password:String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -90,8 +91,20 @@ app.get("/register",function(req,res){
 });
 
 app.get("/secrets",function(req,res){
+  User.find({"secret":{$ne:null}},function(err,foundUsers){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUsers){
+        res.render("secrets",{usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit",function(req,res){
   if (req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   }else{
     res.redirect("/login");
   }
@@ -128,6 +141,22 @@ app.post("/login",function(req,res){
       passport.authenticate("local")(req,res,function(){
         res.redirect("/secrets");
       });
+    }
+  });
+});
+
+app.post("/submit",function(req,res){
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id,function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
     }
   });
 });
